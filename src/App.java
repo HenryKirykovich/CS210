227e6ead7d392e30d75eeffcc41c15250ee3dc34
HenryKirykovich <145735504+HenryKirykovich@users.noreply.github.com>
2025-05-06 15:31:10 -0700

    rewrise logic of code

--- src/App.java
+++ src/App.java
@@ -4,41 +4,50 @@ import java.util.Scanner;
 public class App {
 	// Henadzi Kirykovich CS 210
 	// 6/5/2025
-	// Description: 
+	// Description: Program for check ID by using condition 
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);  // external method for save input inside var
 		boolean choice = false;
-		int counter = 0;
+		
 		int minLength, maxLength;
+		System.out.print("Input min length of ID: ");
+		minLength = input.nextInt();
+		System.out.print("Input max length of ID: ");
+		maxLength = input.nextInt();
 								
-		while (choice==false){
+		while (!choice) { // loop until a valid ID is created
+
+		
 			
-			System.out.print("Input min length of ID:");
-			minLength = input.nextInt();
-			System.out.print("Input max length of ID:");
-			maxLength = input.nextInt();
+		
+			System.out.println();
+			input.nextLine(); // holder for new line
+		
+			System.out.print("Create a new ID: ");
+			String newId = input.next();
+		
 			
-			counter++;
-			input.nextLine();
-			
-			
-			System.out.print("Create a new ID:");
-			String newId = input.nextLine();
-			
-			if (newId.length() >= minLength && newId.length() <= maxLength &
-			newId.charAt(0) == newId.charAt(newId.length() - 1) )     
-			{
-				System.out.println(newId +" "+ "has been sucessfully created!");
-				System.out.println("Number of attempts: " + counter);
-				choice = true;
-			} else
-			{
-				System.out.println("ID must be greater than or equal to 3 and less or equal to 5");
-				System.out.println("The first character and the last character must be the same");
-				System.out.println("Number of attempts: " + counter);
+		
+			if (newId.length() < minLength || newId.length() > maxLength) {
+				System.out.println("ID length must be between " + minLength + " and " + maxLength + ".");
+				choice = false;
 			}
-			
+		
+			if (newId.charAt(0) != newId.charAt(newId.length() - 1)) {
+				System.out.println("First and last characters must be the same.");
+				choice = false;
+			}
+		
+			if (newId.length() % 2 == 0) {
+				System.out.println("ID length must be an odd number.");
+				choice = false;
+			} else  {
+				System.out.println(newId + " has been successfully created!");
+				choice = true;
+			}
 		}
+	
+		
 	}
 }
